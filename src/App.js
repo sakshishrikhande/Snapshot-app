e08f63da0be7f8de141bdd76a7ada1fb021df244
sakshi shrikhande <shrikhandesakshi@gmail.com>
2023-03-25 20:00:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const searchData = useRef(null);
+  const [searchText, setSearchText] = useState("mountains");
+  const [imageData, setImageData] = useState([])
+  useEffect(()=>{
+    const params = {
+      method : "flickr.photos.search",
+      api_key : "6888754a0a17a15d4a1da21ef4e9b84a",
+      text : searchText,
+      sort: "",
+      per_page: 40,
+      license : '4',
+      extras : "owner_name, license",
+      format : "json",
+      nojsoncallback : 1
+    }
+    const parameters = new URLSearchParams(params);
+    const url = `https://api.flickr.com/services/rest/?${parameters}`
+    axios.get(url).then((resp)=>{
+      console.log(resp.data)
+      const arr = resp.data.photos.photo.map((imgData)=>{
+        return fetchFlickrImageUrl(imgData, 'q');
+
+      });
+      setImageData(arr);
+
+    }).catch(()=>{
+
+    }).finally(()=>{
+
+    })
+    
+    
+  },[searchText])
+
+  const fetchFlickrImageUrl = (photo, size)=>{
+    let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`
+    if(size){
+      url += `_${size}`
+    }
+    url += '.jpg'
+    return url
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <i><header className='snapshot'>SnapShot</header></i>
+     <input onChange={(e)=>{searchData.current = e.target.value} }/>
+     <button className='button' onClick={()=>{setSearchText(searchData.current)}}>search</button>
+     <section className='buttons'>
+        <button className='button' onClick={()=> {setSearchText("mountains")}}>Mountains</button>
+        <button className='button' onClick={()=> {setSearchText("beaches")}}>Beaches</button>
+        <button className='button' onClick={()=> {setSearchText("birds")}}>Birds</button>
+        <button className='button' onClick={()=> {setSearchText("foods")}}>Food</button>
+     </section>
+     <section className='image-container'>
+     
+          {imageData.map((imageurl,key) =>{
+            return (
+              <article className='flickr-image'>
+              <img src={imageurl} key={key}/>
+              </article>
+            )
+            
+            
+          })}
+       
+     </section>
     </div>
   );
 }
